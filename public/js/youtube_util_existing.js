d746a31958b9507f3598fe7628be135560928c5d
Calvin Lang & Steven Jones <pair+calvinlang.sjones88@devbootcamp.com>
2016-02-04 17:20:40 -0600

    YouTube videos working

--- public/js/youtube_util_existing.js
+++ public/js/youtube_util_existing.js
@@ -6,36 +6,70 @@ function youtubeParser(url){
 };
 
 
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-$(document).ready(function() {
-  console.log("BLAH")
-  var userLink = youtubeParser($("span.youtube-link").text());
-    // var userLink = youtubeParser($(".youtube-link").val());
-    console.log(userLink)
-  var player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: userLink,
-  iv_load_policy: 3,
-  showinfo:0,
-  //videoId: 'Fj73JF_bhjc',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-
-    function onPlayerReady(event) {
-
-    }
-
-    function onPlayerStateChange(event) {
-      console.log(player.getCurrentTime())
-      // console.log(player.getDuration():Number);
-    }
-
+$( document ).ready(function() {
+    console.log( "ready!" );
+    loadPlayer();
 });
+
+function youTubeLink(){
+  return youtubeParser($(".youtube-link").text());
+}
+
+
+function loadPlayer() {
+  if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  window.onYouTubePlayerAPIReady = function() {
+    onYouTubePlayer();
+    };
+  }
+}
+
+var player;
+
+function onYouTubePlayer() {
+  player = new YT.Player('player', {
+    height: '490',
+    width: '880',
+    videoId: youTubeLink(),
+    playerVars: { controls:1, showinfo: 0, rel: 0, showsearch: 0, iv_load_policy: 3 },
+    events: {
+      'onStateChange': onPlayerStateChange,
+      'onError': catchError
+    }
+  });
+}
+
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      // setTimeout(stopVideo, 6000);
+      done = true;
+    }
+    else if(event.data == YT.PlayerState.ENDED)
+    {
+      location.reload();
+    }
+  }
+
+  function onPlayerReady(event) {
+
+    //if(typeof(SONG.getArtistId()) == undefined)
+    //{
+    //  console.log("undefineeeed");
+    //}
+    //event.target.playVideo();
+  }
+  function catchError(event)
+  {
+    if(event.data == 100) console.log("De video bestaat niet meer");
+  }
+
+  function stopVideo() {
+    player.stopVideo();
+  }
