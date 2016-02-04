a4fa8f993d833aeb0cb0a03ac52a1a1cf89f6d51
sjones88 <steven.g.jones88@gmail.com>
2016-02-04 13:16:51 -0600

    Modify gemfile and environment. Create client model and clients table.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Require gems we care about
 require 'rubygems'
+require 'google/api_client'
+require 'trollop'
 
 require 'uri'
 require 'pathname'
