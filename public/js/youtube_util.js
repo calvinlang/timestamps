ccdc50c127e619768423ced45bfb88bec7a97881
Calvin Lang <langcalvin@gmail.com>
2016-02-04 14:43:11 -0600

    YouTube video loader working

---
+++ public/js/youtube_util.js
@@ -0,0 +1,44 @@
+
+function youtubeParser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+};
+
+console.log( youtubeParser("http://www.youtube.com/watch?v=0zM3nApSvMg#t=0m10s") )
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+$(document).ready(function() {
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+
+    var userLink = youtubeParser($(".youtube-link").val());
+
+    var player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: userLink,
+  iv_load_policy: 3,
+  showinfo:0,
+  //videoId: 'Fj73JF_bhjc',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+
+    function onPlayerReady(event) {
+
+    }
+
+    function onPlayerStateChange(event) {
+      console.log(player.getCurrentTime())
+      // console.log(player.getDuration():Number);
+    }
+  });
+});
+  
