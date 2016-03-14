2dd4abf696c2180fed5c338e1d041dfa8b699db0
Calvin Lang <langcalvin@gmail.com>
2016-03-13 21:16:11 -0500

    Main page CSS parts done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+
+  def uid
+  end
+
+  def new
+    @user = User.new
+    render "sessions/new", layout: false
+  end
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      @session = session[:user_id]
+      respond_to do |format|
+        format.js { render nothing: true }
+        format.html { render nothing: true }
+      end
+    else
+      return 406
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    respond_to do |format|
+        format.html { render nothing: true }
+    end
+  end
+
+  private
+    def login_params
+      params.require(:user).permit(:username, :password)
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new()
+    render "users/new", layout: false
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      respond_to do |format|
+        format.js { render nothing: true }
+        format.html { render nothing: true }
+      end
+      return 202
+    else
+      return 406
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_user
+      @user = User.find(params[:id])
+  end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :username, :avatar_url)
+  end
+end
